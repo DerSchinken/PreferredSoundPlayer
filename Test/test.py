e27f9106ff3f7880d233ff17cf67c4cef9a02699
Paul Hippler <paulhippler21@gmail.com>
2023-11-07 14:12:42 +0100

    nope

--- Test/test.py
+++ Test/test.py
@@ -1,4 +1,5 @@
 import os
+print(os.getcwd())
 print(os.listdir("."))
 print(list(os.walk(".")))
 from preferredsoundplayer import soundplay, stopsound, loopsound, stoploop, get_is_playing, get_is_loop_playing
