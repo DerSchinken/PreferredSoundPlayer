e42c727bf344981f15d2ab5a258396dc2083c4ee
Paul Hippler <paulhippler21@gmail.com>
2023-11-07 13:43:49 +0100

    added quick and dirty tests

--- preferredsoundplayer/__init__.py
+++ preferredsoundplayer/__init__.py
@@ -1,4 +1,4 @@
 from preferredsoundplayer.preferredsoundplayer import (
     soundplay, playwave, stopsound, stopwave, loopsound,
-    loopwave, get_is_playing, get_is_loop_playing
+    loopwave, stoploop, get_is_playing, get_is_loop_playing
 )
