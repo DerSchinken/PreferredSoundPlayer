4f2cbe98946b33604876bc0ff17f44ca97a76051
Paul Hippler <paulhippler21@gmail.com>
2023-11-07 09:40:55 +0100

    added typhinting; cleaned up code (automatic reformat with pycharm); fixed some typos; made code more pep8 conform

--- preferredsoundplayer/__init__.py
+++ preferredsoundplayer/__init__.py
@@ -1,1 +1,4 @@
-from preferredsoundplayer.preferredsoundplayer import *
+from preferredsoundplayer.preferredsoundplayer import (
+    soundplay, playwave, stopsound, stopwave, loopsound,
+    loopwave, get_is_playing, get_is_loop_playing
+)
